4673f1f3528cfe2f62124a77214aaa6cf03ead95
Dez <dezlin77@users.noreply.github.com>
2019-03-11 16:35:24 +0800

    fizzbuzz

---
+++ fizzbuzz.py
@@ -0,0 +1,16 @@
+'''
+Write a program that prints the numbers from 1 to 100.
+But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”.
+For numbers which are multiples of both three and five print “FizzBuzz”
+'''
+
+for x in range(1,101):
+	s = ""
+	if x % 3 == 0:
+		s += "Fizz"
+	if x % 5 == 0:
+		s += "Buzz"
+	if s == "":
+		s = x
+	print (s)
+
