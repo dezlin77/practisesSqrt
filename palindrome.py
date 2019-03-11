a629d9303bcd570bf763764b9940c4357dc3db1c
Dez <dezlin77@users.noreply.github.com>
2019-03-11 11:46:12 +0800

    find a palindromein a string, no need punctuation

---
+++ palindrome.py
@@ -0,0 +1,27 @@
+'''
+Palindrome is a word that can be read front and back the same
+i.e. "Able was I" and "I was able"
+
+First, pull out all punctuation, spaces
+2nd, convert all to lower case
+3rd, if first and last is the same, check the letters in between.
+'''
+
+def isPalindrome(s):
+	def toChars(s):
+		s = s.lower()
+		ans = ''
+		for c in s:
+			if c in "abcdefghijklmnopqrstuvwxyz":
+				ans = ans + c
+		return ans
+
+	def isPal(s):
+		if len(s) <= 1:
+			return True
+		else:
+			return s[0] == s[-1] and isPal(s[1:-1])
+
+	return isPal(toChars(s))
+
+print(isPalindrome("abeeba"))
